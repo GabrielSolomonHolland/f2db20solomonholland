5e66e21480e94ad984973d204a0c9fdd60377d91
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-11-10 17:35:09 -0600

    update controller with getdetail

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -13,10 +13,16 @@ exports.vehicle_list = async function(req, res) {
     }
 }; 
 
-// for a specific vehicle
-exports.vehicle_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: vehicle detail: ' + req.params.id); 
-}; 
+exports.vehicle_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Vehicle.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};
  
 exports.vehicle_create_post = async function(req, res) { 
     console.log(req.body) 

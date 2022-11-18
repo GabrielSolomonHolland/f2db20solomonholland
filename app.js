5ac39f442e06502f85dcdecfd26462dda165b1dd
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-11-18 10:46:49 -0600

    initialize authentication through passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -22,9 +24,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/vehicle',vehicleRouter)
 app.use('/routes',resourceRouter)
+
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 
 
 require('dotenv').config(); 
@@ -46,6 +71,15 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 // error handler
 app.use(function(err, req, res, next) {

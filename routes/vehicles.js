1b2bf1fa191ab2c2dde12fd0b2fde121f2837208
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-11-30 22:08:42 -0600

    finish ss 6

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -7,8 +7,17 @@ router.get('/', vehicle_controllers.vehicle_view_all_Page);
 router.get('/detail', vehicle_controllers.vehicle_view_one_Page);
 
 router.get('/create', vehicle_controllers.vehicle_create_Page);
+ 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
-router.get('/update', vehicle_controllers.vehicle_update_Page);
+router.get('/update', secured, vehicle_controllers.vehicle_update_Page);
 
 router.get('/delete', vehicle_controllers.vehicle_delete_Page);
 

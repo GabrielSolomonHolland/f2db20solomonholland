395298f6f3e9a95d4de2a5141a037795e76968ba
GabrielSolomonHolland <69911029+GabrielSolomonHolland@users.noreply.github.com>
2022-11-30 22:18:17 -0600

    finish assignment

--- models/vehicle.js
+++ models/vehicle.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({ 
  model: String, 
  mileage: Number, 
- numWheels: Number 
-}) 
+ numWheels: {
+    type: Number,
+    min: 0,
+    max: 10}
+ }) 
  
 module.exports = mongoose.model("Vehicle", 
 vehicleSchema) 
--- routes/resource.js
+++ routes/resource.js
@@ -33,7 +33,7 @@ router.get('/create', vehicle_controller.vehicle_create_Page);
 /* GET create update page */ 
 router.get('/update', vehicle_controller.vehicle_update_Page);
 
-/* GET delete bike page */
+/* GET delete page */
 router.get('/delete', vehicle_controller.vehicle_delete_Page);
  
 module.exports = router; 
